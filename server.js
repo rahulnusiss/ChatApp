6a9039ada6615b67adcaf1f887ebce56f6e5dc9b
rahulnusiss <rahulnusiss@gmail.com>
2017-04-20 17:56:44 +0800

    Chat working, final finish remaining

---
+++ server.js
@@ -0,0 +1,35 @@
+var mongo = require('mongodb').MongoClient,
+	client = require('socket.io').listen(8080).sockets;
+
+	mongo.connect('mongodb://127.0.0.1/chat',function(err, db){
+		if(err) throw err;
+
+		client.on('connection', function(socket){
+			var col = db.collection('messages'),
+			sendStatus = function(s){
+				socket.emit('status',s);
+			};
+
+			// Emit all messages
+			col.find().limit(100).sort({_id:1}).toArray(function(err,res){
+				if(err) throw err;
+				socket.emit('output', res);
+			});
+		// wait for input.
+		socket.on('input', function(data){				
+				var name = data.name;
+				message = data.message;
+				col.insert({name: name, message: message}, function(){
+
+					client.emit('output', [data]);
+					sendStatus({
+						message: "Message sent",
+						clear: true
+					});
+					console.log('Inserted');
+				});
+			});
+		});
+	});
+
+	
